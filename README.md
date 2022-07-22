119b879b8f11ae0db1eef4de350e8c3804388502
serjjs <shs@trueflip.io>
2022-07-22 18:08:24 +0300

    added mobile adaptive, cdn JS

--- README.md
+++
@@ -1,1 +0,0 @@
-# experemental-port
---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* typing animation */
+
+let typed = new Typed('.typing', {
+    strings: ['', 'React Developer', 'Web Developer', 'Vue Developer', 'Frontend Developer', 'JavaScript Developer'],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
