5ec88e2bf77c375ca75fe134af3339fa9dd46daa
serjjs <shs@trueflip.io>
2022-11-28 18:59:19 +0300

    begin

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ let typed = new Typed('.typing', {
     BackSpeed: 60,
     loop: true
 });
+
+/* aside */
+const nav = document.querySelector('.nav');
+const navList = document.querySelectorAll('li');
+const totalNavList = navList.length;
+
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector('a');
+    a.addEventListener('click', function() {
+        for (let j = 0; j < totalNavList; j++) {
+            navList[j].querySelector('a').classList.remove('active');
+        }
+        this.classList.add('active');
+        showSection(this);
+    })
+}
+
+function showSection(element) {
+    console.log(element.getAttribute('href').split('#'))
+}
